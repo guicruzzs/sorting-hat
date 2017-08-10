e13f75045ce7b77fdaa996330a5ad1224632cdb5
Guilherme Cruz <guicruz.zs@gmail.com>
2017-08-10 19:57:31 -0300

    Show the not rated tracks first at homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @tracks = TrackSearcher.call(current_user.id, params[:page])
+    @tracks = TrackSearcher.call_not_evaluated_preferably(current_user.id)
     @ranking_tracks = RankingGetter.call
 
     render layout: 'home_dashboard'
